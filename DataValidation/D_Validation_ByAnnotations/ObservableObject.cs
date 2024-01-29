78f18d6e3e76995176ddf7c00269b61268227921
Frederick Javalera <fjavalera@goldpointsystems.com>
2024-01-29 09:50:49 -0700

    Formatting.

--- DataValidation/D_Validation_ByAnnotations/ObservableObject.cs
+++ DataValidation/D_Validation_ByAnnotations/ObservableObject.cs
@@ -8,38 +8,38 @@ using System.Threading.Tasks;
 
 namespace D_Validation_ByAnnotations
 {
+  /// <summary>
+  /// A base for objects using property notification.
+  /// </summary>
+  public class ObservableObject : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
     /// <summary>
-    /// A base for objects using property notification.
+    /// Notify a property change
     /// </summary>
-    public class ObservableObject : INotifyPropertyChanged
+    /// <param name="propertyName">Name of property to update</param>
+    protected virtual void OnPropertyChanged(string propertyName)
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Notify a property change
-        /// </summary>
-        /// <param name="propertyName">Name of property to update</param>
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        /// <summary>
-        /// Notify a property change that uses CallerMemberName attribute
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="backingField">Backing field of property</param>
-        /// <param name="value">Value to give backing field</param>
-        /// <param name="propertyName"></param>
-        /// <returns></returns>
-        protected virtual bool OnPropertyChanged<T>(ref T backingField, T value, [CallerMemberName] string propertyName = "")
-        {
-            if (EqualityComparer<T>.Default.Equals(backingField, value))
-                return false;
-
-            backingField = value;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
+    /// <summary>
+    /// Notify a property change that uses CallerMemberName attribute
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="backingField">Backing field of property</param>
+    /// <param name="value">Value to give backing field</param>
+    /// <param name="propertyName"></param>
+    /// <returns></returns>
+    protected virtual bool OnPropertyChanged<T>(ref T backingField, T value, [CallerMemberName] string propertyName = "")
+    {
+      if (EqualityComparer<T>.Default.Equals(backingField, value))
+        return false;
+
+      backingField = value;
+      OnPropertyChanged(propertyName);
+      return true;
+    }
+  }
 }
