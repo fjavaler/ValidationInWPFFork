ac591917921a9b3007d06a8f0aa026fac5a76763
Frederick Javalera <fjavalera@goldpointsystems.com>
2024-01-29 09:55:22 -0700

    Adding documentation.

--- DataValidation/D_Validation_ByAnnotations/RegistrationVM.cs
+++ DataValidation/D_Validation_ByAnnotations/RegistrationVM.cs
@@ -9,8 +9,14 @@ namespace D_Validation_ByAnnotations
 {
   public class RegistrationVM : ObservableObject
   {
+    /// <summary>
+    /// Username backing field.
+    /// </summary>
     private string _username;
 
+    /// <summary>
+    /// Username property with validation attributes.
+    /// </summary>
     [Required(ErrorMessage = "Must not be empty.")]
     [StringLength(50, MinimumLength = 5, ErrorMessage = "Must be at least 5 characters.")]
     public string Username
@@ -23,6 +29,12 @@ namespace D_Validation_ByAnnotations
       }
     }
 
+    /// <summary>
+    /// Property validation method.
+    /// </summary>
+    /// <typeparam name="T">Generic type.</typeparam>
+    /// <param name="value">The value to validate.</param>
+    /// <param name="name"></param>
     private void ValidateProperty<T>(T value, string name)
     {
       Validator.ValidateProperty(value, new ValidationContext(this, null, null)
